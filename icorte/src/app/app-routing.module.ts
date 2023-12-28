08289377027a10ecf7abc81f27dd9b8ad1418464
Guga <gugatascheri@gmail.com>
2023-12-28 13:41:15 -0300

    Atualiza;'oes

--- icorte/src/app/app-routing.module.ts
+++ icorte/src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import  {redirectUnauthorizedTo,
+redirectLoggedInTo,
+
+canActivate} from '@angular/fire/auth-guard';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule),
+    ...canActivate()
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule),
+    ...canActivate()
+  },
+  {
+    path: 'cadastro',
+    loadChildren: () => import('./cadastro/cadastro.module').then( m => m.CadastroPageModule)
   }
+
 ];
 @NgModule({
   imports: [
--- icorte/src/app/app.module.ts
+++ icorte/src/app/app.module.ts
@@ -6,10 +6,20 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports:
+   [BrowserModule,
+     IonicModule.forRoot(),
+      AppRoutingModule,
+       provideFirebaseApp(() => 
+       initializeApp({"projectId":"icorte-5cc11",
+       "appId":"1:575713557136:web:cf0cf6ffb31f415f8bf615","storageBucket":"icorte-5cc11.appspot.com","apiKey":"AIzaSyD2EBvWefIqpQJj-ueARMOckRqPw5ziOYg","authDomain":"icorte-5cc11.firebaseapp.com","messagingSenderId":"575713557136","measurementId":"G-JNWVZ7YL5Y"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
