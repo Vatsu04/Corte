9aaba90775cd90fe65dc6b9710cac4ce8c285fba
Guga <gugatascheri@gmail.com>
2024-01-02 13:16:43 -0300

    atu

--- icorte/src/app/services/auth.service.ts
+++ icorte/src/app/services/auth.service.ts
@@ -1,13 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut  } from '@angular/fire/auth';
-
+import { Firestore, doc, docData } from '@angular/fire/firestore';
+import {Photo} from '@capacitor/camera';
+import {Storage} from '@angular/fire/storage';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private auth: Auth) {}
+  constructor(
+    private auth: Auth,
+    private firestore: Firestore,
+    private storage: Storage
+    ) {}
 
+
+    getUserProfile(){
+      const user = this.auth.currentUser;
+      const userDocRef = doc(this.firestore, `users/${user.uid}`);
+      return docData(userDocRef);
+    }
+    
   async register ({email, password}: { email: string, password: string }) {
     try {
       const user = await createUserWithEmailAndPassword(
--- icorte/src/app/tab1/tab1.page.ts
+++ icorte/src/app/tab1/tab1.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AvatarService } from '../services/avatar.service';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { AlertController, LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +10,20 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  profile=null;
+  constructor(
+    private avatarService:AvatarService,
+    private authService:AuthService,
+    private router: Router,
+    private loadingController: LoadingController,
+    private alertController: AlertController
+  ) {}
 
-  constructor() {}
+  async logout(){
+   await this.authService.logout()
+   this.router.navigateByUrl('/', {replaceUrl: true});
+   
+  }
 
+  async changeImage(){}
 }
