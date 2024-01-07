c7a0a653c2be6bbda6a2e9b30dbeca4ecaa0a547
Guga <gugatascheri@gmail.com>
2024-01-07 19:04:34 -0300

    Update

--- icorte/src/app/tab2/tab2.page.ts
+++ icorte/src/app/tab2/tab2.page.ts
@@ -6,7 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
+  barbeiros:any = [{nome:''.}]
   constructor() {}
 
+
+  async listarBanco() {
+    const querySnapshot = await getDocs(collection(this.firestore, "Barbeiros"));
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data()['nome']}`);
+      this.barbeiros = [...this.barbeiros, { nome: doc.data()['nome'], descricao: doc.data()['descricao'], preco: doc.data()['preco'], qtd: doc.data()['qtd'], image: doc.data()['image'] }]
+    });
+  }
 }
