57d02955a2228b5f8eee1078fe7816393f6bdb2f
Guga <gugatascheri@gmail.com>
2024-01-25 11:04:50 -0300

    Update

--- icorte/src/app/pedidos-completos-cliente/pedidos-completos-cliente.page.ts
+++ icorte/src/app/pedidos-completos-cliente/pedidos-completos-cliente.page.ts
@@ -1,8 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { Firestore, collection, doc, getDoc, getDocs } from '@angular/fire/firestore';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Firestore, collection, doc, getDoc, getDocs, updateDoc } from '@angular/fire/firestore';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+
+enum COLORS {
+  GREY = "#E0E0E0",
+  GREEN = "#76FF03",
+  YELLOW = "#FFCA28",
+  RED = "#DD2C00"
+}
 
 @Component({
   selector: 'app-pedidos-completos-cliente',
@@ -13,22 +20,60 @@ export class PedidosCompletosClientePage implements OnInit {
   teste:any = [];
   pedidos:any = [];
   usuarios:any = [];
+  @Input() rating: number;
+  @Output() ratingChange: EventEmitter<number> = new EventEmitter();
   credentials: FormGroup = this.fb.group({
-    rapidez: ['', [Validators.required, ]],
-    organizacao: ['', [Validators.required, ]],
-    educacao: ['', [Validators.required, ]]
+    avaliacao: ['', [Validators.required, ]],
+
   });
   constructor(
+  
     private firestore: Firestore,
     private fb: FormBuilder,
     private authService: AuthService,
     private router: Router
 
-  ) { }
+  ) {
+    this.rating =0;
+   }
 
   async ngOnInit() {
     await this.listarBanco();
     this.listarPedidos();
+  }
+
+  async rate(index: number, id:string) {
+    this.rating = index;
+    this.ratingChange.emit(this.rating);
+    try {
+      await updateDoc(doc(this.firestore, 'pedidos_feitos', id), { avaliacao: this.rating });
+      console.log('Avaliacao updated successfully!');
+    } catch (error) {
+      console.error('Error updating avaliacao:', error);
+    }
+  }
+
+  getColor(index: number) {
+    if (this.isAboveRating(index)) {
+      return COLORS.GREY;
+    }
+
+    switch (this.rating) {
+      case 1:
+      case 2:
+        return COLORS.RED;
+      case 3:
+        return COLORS.YELLOW;
+      case 4:
+      case 5:
+        return COLORS.GREEN;
+      default:
+        return COLORS.GREY;
+    }
+  }
+
+  isAboveRating(index: number): boolean {
+    return index > this.rating;
   }
   async listarPedidos() {
     
@@ -55,8 +100,8 @@ export class PedidosCompletosClientePage implements OnInit {
     }
   }
   }
-  get rapidez(){
-    return this.credentials.get('rapidez');
+  get avaliacao(){
+    return this.credentials.get('avaliacao');
   }
 
   get organizao(){
