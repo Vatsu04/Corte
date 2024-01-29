aabe4c6059cfa0a14b98a6750e8620f4276ac548
Guga <gugatascheri@gmail.com>
2024-01-29 12:14:20 -0300

    Update editar-conta-barbeiro.page.ts

--- icorte/src/app/editar-conta-barbeiro/editar-conta-barbeiro.page.ts
+++ icorte/src/app/editar-conta-barbeiro/editar-conta-barbeiro.page.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Auth, updatePassword } from '@angular/fire/auth';
+import { Auth, updatePassword , getAuth, onAuthStateChanged, updateEmail} from '@angular/fire/auth';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-editar-conta-barbeiro',
@@ -27,10 +28,22 @@ export class EditarContaBarbeiroPage implements OnInit {
     private authService: AuthService,
     private fb: FormBuilder,
     private router: Router,
+    private toastController: ToastController,
     private auth:Auth
   ) {}
 
   ngOnInit() {}
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 3000,
+      position: 'bottom',
+      color: 'dark', // You can change the color based on your preference
+    });
+
+    toast.present();
+  }
 
 
   get email() {
@@ -69,13 +82,20 @@ export class EditarContaBarbeiroPage implements OnInit {
   
 
   async editarPerfil(perfilAtualizado: any) {
-    this.editUserProfile(perfilAtualizado);
-    this.editUserPassword(perfilAtualizado);
+    this.editBarberProfile(perfilAtualizado);
+    this.editBarberPassword(perfilAtualizado);
+
+   /*  if (success) {
+      this.presentToast('User profile updated successfully');
+
+    } else {
+      this.presentToast('Failed to update user profile');
+    } */
   }
 
 
   
-  async editUserProfile(updatedProfile: any) {
+  async editBarberProfile(updatedProfile: any) {
     const uid = await this.authService.getCurrentUserUID();
 
     if (uid) {
@@ -99,7 +119,7 @@ export class EditarContaBarbeiroPage implements OnInit {
     }
   }
 
-  async editUserPassword(updatedProfile: any) {
+  async editBarberPassword(updatedProfile: any) {
     const email = updatedProfile.email;
     const oldPassword = updatedProfile.oldPassword;
     const newPassword = updatedProfile.newPassword;
